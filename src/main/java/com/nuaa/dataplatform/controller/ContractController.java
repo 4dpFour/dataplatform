5ce5cb1921ff8645dab100a260f0035cd6f8786e
EEE12315 <47921191+EEE12315@users.noreply.github.com>
2020-11-23 22:50:32 +0800

    first commit ContractService

--- src/main/java/com/nuaa/dataplatform/controller/ContractController.java
+++ src/main/java/com/nuaa/dataplatform/controller/ContractController.java
@@ -7,8 +7,28 @@ import com.nuaa.dataplatform.util.ResultCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
 @RequestMapping("/contract")
 public class ContractController {
+
+    @Autowired
+    private ContractService contractService;
+
+    @GetMapping("/{id}")
+    public Result getContractNo(@PathVariable int id) {
+        try {
+            Contract contract = contractService.getContract(id);
+            if (contract != null) {
+                return Result.success(contract);
+            } else {
+                return Result.failure(ResultCode.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.failure(ResultCode.SERVER_ERROR);
+        }
+    }
 
 }
--- src/main/java/com/nuaa/dataplatform/service/ContractService.java
+++ src/main/java/com/nuaa/dataplatform/service/ContractService.java
@@ -5,6 +5,8 @@ import com.nuaa.dataplatform.entity.Contract;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class ContractService {
 
@@ -13,6 +15,13 @@ public class ContractService {
 
     public Contract getContract(int id) {
         return contractDAO.selectById(id);
+    }
+
+    public Contract addContract(String contractNo, String contractName, String projectNo, String projectName,
+                                String purchaser, String purchaserTelNo, String supplier, String supplierTelNo,
+                                String subjectName, float subjectUnitPrice, float contractValue, Date announceDate)
+    {
+        return null;
     }
 
     public void updateUser(Contract contract) {
